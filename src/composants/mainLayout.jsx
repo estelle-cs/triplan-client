fe23d5b901398c3bd353336a399683f69a93d255
Estelle165 <estelle@lesfabricants.io>
2024-07-04 23:06:00 +0200

    new : main layout

---
+++ src/composants/mainLayout.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import {
+  LaptopOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme, ConfigProvider } from "antd";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import { Avatar, Space } from "antd";
+
+function MainLayout() {
+  const { Header, Content, Sider } = Layout;
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const items = [
+    {
+      key: "sub1",
+      icon: <LaptopOutlined />,
+      label: "Mes trips",
+      children: [
+        {
+          key: "1",
+          label: <Link to="/newTrip">Nouveau trip</Link>,
+          link: "/newTrip",
+        },
+        {
+          key: "2",
+          label: <Link to="/dashboard">Voir mes trips</Link>,
+          link: "/dashboard",
+        },
+      ],
+    },
+    {
+      key: "sub2",
+      icon: <UserOutlined />,
+      label: "Utilisateur",
+      children: [
+        { key: "3", label: <Link to="/profil">Profil</Link>, link: "/profil" },
+        {
+          key: "4",
+          label: <Link to="/logout">Déconnexion</Link>,
+          link: "/logout",
+        },
+      ],
+    },
+  ];
+
+  const selectedKeys = items.reduce((keys, item) => {
+    item.children.forEach((child) => {
+      if (child.link === currentPath) {
+        keys.push(child.key);
+      }
+    });
+    return keys;
+  }, []);
+
+  const openKeys = items.reduce((keys, item) => {
+    item.children.forEach((child) => {
+      if (child.link === currentPath) {
+        keys.push(item.key);
+      }
+    });
+    return keys;
+  }, []);
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Menu: {
+            itemSelectedColor: "var(--dark)",
+            itemSelectedBg: "var(--light-hover)",
+          },
+        },
+      }}
+    >
+      <Layout style={{ height: "100vh" }}>
+        <Header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            position: "fixed",
+            zIndex: 1,
+            width: "100%",
+            backgroundColor: "var(--even-darker)",
+          }}
+        >
+          <Link to="/home"><span className="title-s logo">TRIPLAN</span></Link>
+          <Link to="/profil">
+            <Avatar size="large" icon={<UserOutlined />} />
+          </Link>
+        </Header>
+        <Layout style={{ marginTop: 64 }}>
+          <Sider
+            width={200}
+            style={{
+              background: "var(--normal)",
+              height: "calc(100vh - 64px)",
+              position: "fixed",
+              left: 0,
+              top: 64,
+              bottom: 0,
+            }}
+          >
+            <Menu
+              mode="inline"
+              selectedKeys={selectedKeys}
+              defaultOpenKeys={openKeys}
+              style={{
+                height: "100%",
+                borderRight: 0,
+              }}
+              items={items}
+            />
+          </Sider>
+          <Layout style={{ marginLeft: 200, padding: "24px" }}>
+            <Content
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+                overflow: "auto",
+                height: "calc(100vh - 64px - 24px)",
+              }}
+            >
+              <Outlet /> {}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
+  );
+}
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import NoPage from './pages/no-page/no-page.jsx'
 import Login from './pages/login/login.jsx'
 import TripDetails from './pages/tripDetail/trip-details.jsx'
 import TripDashboard from './pages/tripDashboard/trip-dashboard.jsx'
+import NewTrip from './pages/newTrip/newTrip.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MainLayout from "./composants/mainLayout.jsx"
 import 'antd/dist/reset.css';
 import './main.css'
 
@@ -14,12 +16,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="newTrip" element={<NewTrip />} />
+              <Route path="trip" element={<TripDetails />} />
+              <Route path="dashboard" element={<TripDashboard />} />
+              <Route path="*" element={<NoPage />} />
+          </Route>
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/trip" element={<TripDetails />} />
-          <Route path="/dashboard" element={<TripDashboard />} />
       </Routes>
     </Router>
 </React.StrictMode>,
