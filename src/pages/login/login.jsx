45505f667bbdb6df9e377a8fc622209b59a7c9ca
Estelle165 <estelle@lesfabricants.io>
2024-07-05 01:46:04 +0200

    improve : login and register front

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,54 +1,56 @@
-import React, { useState } from 'react';
-import './login.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./login.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
- 
-export const setAuthToken = token => {
-   if (token) {
-       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   }
-   else
-       delete axios.defaults.headers.common["Authorization"];
-}
+export const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else delete axios.defaults.headers.common["Authorization"];
+};
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({ email: '', password: '' });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ email: "", password: "" });
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
-    setErrors({ ...errors, email: '' });
-    };
+    setErrors({ ...errors, email: "" });
+  };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-    setErrors({ ...errors, password: '' });
+    setErrors({ ...errors, password: "" });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!isValidEmail(email)) {
-      setErrors({ ...errors, email: 'Veuillez entrer une adresse email valide' });
+      setErrors({
+        ...errors,
+        email: "Veuillez entrer une adresse email valide",
+      });
       return;
     }
     if (!password) {
-      setErrors({ ...errors, password: 'Veuillez entrer votre mot de passe' });
+      setErrors({ ...errors, password: "Veuillez entrer votre mot de passe" });
       return;
     }
-    const loginInfo = {email: email, password: password};
-    axios.post('http://localhost:3000/auth/login', loginInfo).then(response => {
-      const token  =  response.data.access_token;
-      localStorage.setItem("token", token);
-      setAuthToken(token);
-      window.location.href = '/dashboard'
-    })
-    .catch(err => console.log(err));
+    const loginInfo = { email: email, password: password };
+    axios
+      .post("http://localhost:3000/auth/login", loginInfo)
+      .then((response) => {
+        const token = response.data.access_token;
+        localStorage.setItem("token", token);
+        setAuthToken(token);
+        window.location.href = "/dashboard";
+      })
+      .catch((err) => console.log(err));
 
-    setEmail('');
-    setPassword('');
-    setErrors({ email: '', password: '' });
+    setEmail("");
+    setPassword("");
+    setErrors({ email: "", password: "" });
   };
 
   const isValidEmail = (email) => {
@@ -57,38 +59,56 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <h2>Connexion</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Adresse Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-          {errors.email && <span className="error-message">{errors.email}</span>}
+    <body className="body-login">
+      <div className="login-container">
+        <h2 className="title-m h2-login">Connexion</h2>
+        <form className="form-login" onSubmit={handleSubmit}>
+          <div className="label-input-div">
+            <label className="body-m label-login" htmlFor="email">
+              Adresse Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="body-s input-login"
+              placeholder="Entrez votre adresse email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+            {errors.email && (
+              <span className="error-message">{errors.email}</span>
+            )}
+          </div>
+          <div className="label-input-div">
+            <label className="body-m label-login" htmlFor="password">
+              Mot de passe:
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="body-s input-login"
+              placeholder="Entrez votre mot de passe"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+            {errors.password && (
+              <span className="error-message">{errors.password}</span>
+            )}
+          </div>
+          <button className="button-submit" type="submit">
+            Se connecter
+          </button>
+        </form>
+        <div className="register-link">
+          <p>
+            Pas encore de compte ? <Link to="/register">Inscrivez-vous</Link>
+          </p>
         </div>
-        <div>
-          <label htmlFor="password">Mot de passe:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-          {errors.password && <span className="error-message">{errors.password}</span>}
-        </div>
-        <button type="submit">Se connecter</button>
-      </form>
-      <div className="register-link">
-        <p>Pas encore de compte ? <Link to="/register">Inscrivez-vous</Link></p>
       </div>
-    </div>
-  )
+    </body>
+  );
 };
 
 export default Login;
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,67 +1,72 @@
-import React, { useState } from 'react';
-import './register.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../login/login.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
- 
-export const setAuthToken = token => {
-   if (token) {
-       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   }
-   else
-       delete axios.defaults.headers.common["Authorization"];
-}
+export const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else delete axios.defaults.headers.common["Authorization"];
+};
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [errors, setErrors] = useState({ email: '', password: '' });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [errors, setErrors] = useState({ email: "", password: "" });
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
-    setErrors({ ...errors, email: '' });
-    };
+    setErrors({ ...errors, email: "" });
+  };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-    setErrors({ ...errors, password: '' });
+    setErrors({ ...errors, password: "" });
   };
 
   const handlePasswordChange2 = (e) => {
     setPassword2(e.target.value);
-    setErrors({ ...errors, password: '' });
+    setErrors({ ...errors, password: "" });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!isValidEmail(email)) {
-      setErrors({ ...errors, email: 'Veuillez entrer une adresse email valide' });
+      setErrors({
+        ...errors,
+        email: "Veuillez entrer une adresse email valide",
+      });
       return;
     }
     if (!password) {
-      setErrors({ ...errors, password: 'Veuillez entrer votre mot de passe' });
+      setErrors({ ...errors, password: "Veuillez entrer votre mot de passe" });
       return;
     }
     if (password !== password2) {
-      setErrors({ ...errors, password: 'Les mots de passe ne correspondent pas' });
+      setErrors({
+        ...errors,
+        password: "Les mots de passe ne correspondent pas",
+      });
       return;
     }
-    const loginInfo = {email: email, password: password};
-    axios.post('http://localhost:3000/auth/register', loginInfo).then(response => {
-      console.log(response)
-      if (response.status === 201) {
-        const token  =  response.data.access_token;
-        localStorage.setItem("token", token);
-        setAuthToken(token);
-        window.location.href = '/'
-      }
-    })
-    .catch(err => console.log(err));
+    const loginInfo = { email: email, password: password };
+    axios
+      .post("http://localhost:3000/auth/register", loginInfo)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 201) {
+          const token = response.data.access_token;
+          localStorage.setItem("token", token);
+          setAuthToken(token);
+          window.location.href = "/";
+        }
+      })
+      .catch((err) => console.log(err));
 
-    setEmail('');
-    setPassword('');
-    setPassword2('');
-    setErrors({ email: '', password: '' });
+    setEmail("");
+    setPassword("");
+    setPassword2("");
+    setErrors({ email: "", password: "" });
   };
 
   const isValidEmail = (email) => {
@@ -70,49 +75,73 @@ const Register = () => {
   };
 
   return (
-    <div className="login-container">
-      <h2>Inscription</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Adresse Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-          {errors.email && <span className="error-message">{errors.email}</span>}
+    <body className="body-login">
+      <div className="login-container">
+        <h2 className="title-m h2-login">Inscription</h2>
+        <form className="form-login" onSubmit={handleSubmit}>
+          <div className="label-input-div">
+            <label className="label-login body-m" htmlFor="email">
+              Adresse Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="body-s input-login"
+              placeholder="Entrez votre adresse email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+            {errors.email && (
+              <span className="error-message">{errors.email}</span>
+            )}
+          </div>
+          <div className="label-input-div">
+            <label className="label-login body-m" htmlFor="password">
+              Mot de passe:
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="body-s input-login"
+              placeholder="Entrez votre mot de passe"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+            {errors.password && (
+              <span className="error-message">{errors.password}</span>
+            )}
+          </div>
+          <div className="label-input-div">
+            <label htmlFor="label-login password">
+              Confirmez votre mot de passe:
+            </label>
+            <input
+              type="password"
+              id="password2"
+              className="body-s input-login"
+              placeholder="Entrez votre mot de passe"
+              value={password2}
+              onChange={handlePasswordChange2}
+              required
+            />
+            {errors.password && (
+              <span className="error-message">{errors.password}</span>
+            )}
+          </div>
+          <button className="button-submit" type="submit">
+            S'inscrire
+          </button>
+        </form>
+        <div className="register-link">
+          <p>
+            Déja un compte ? <Link to="/login">Connectez-vous</Link>
+          </p>
         </div>
-        <div>
-          <label htmlFor="password">Mot de passe:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-          {errors.password && <span className="error-message">{errors.password}</span>}
-        </div>
-        <div>
-          <label htmlFor="password">Confirmez votre mot de passe:</label>
-          <input
-            type="password"
-            id="password2"
-            value={password2}
-            onChange={handlePasswordChange2}
-            required
-          />
-          {errors.password && <span className="error-message">{errors.password}</span>}
-        </div>
-        <button type="submit">S'inscrire</button>
-      </form>
-      <div className="register-link">
-        <p>Déja un compte ? <Link to="/login">Connectez-vous</Link></p>
       </div>
-    </div>
-  )
+    </body>
+  );
 };
 
 export default Register;
